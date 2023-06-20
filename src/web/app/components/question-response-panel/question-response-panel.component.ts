499f580f79edf511e83a945e007595a848297c53
Zong Xun <zxun2000@gmail.com>
2023-06-20 17:00:40 +0800

    fix: flickering bug on session results page

--- src/web/app/components/question-response-panel/question-response-panel.component.ts
+++ src/web/app/components/question-response-panel/question-response-panel.component.ts
@@ -24,14 +24,14 @@ import { FeedbackQuestionModel } from '../../pages-session/session-result-page/s
   styleUrls: ['./question-response-panel.component.scss'],
 })
 export class QuestionResponsePanelComponent {
-
   readonly RESPONSE_HIDDEN_QUESTIONS: FeedbackQuestionType[] = [
     FeedbackQuestionType.CONTRIB,
   ];
 
-  constructor(private feedbackSessionsService: FeedbackSessionsService,
-              private statusMessageService: StatusMessageService) {}
-
+  constructor(
+    private feedbackSessionsService: FeedbackSessionsService,
+    private statusMessageService: StatusMessageService
+  ) {}
   @Input()
   questions: FeedbackQuestionModel[] = [];
 
@@ -65,14 +65,23 @@ export class QuestionResponsePanelComponent {
   previewAsPerson: string = '';
 
   canUserSeeResponses(question: FeedbackQuestionModel): boolean {
-    const showResponsesTo: FeedbackVisibilityType[] = question.feedbackQuestion.showResponsesTo;
+    const showResponsesTo: FeedbackVisibilityType[] =
+      question.feedbackQuestion.showResponsesTo;
     if (this.intent === Intent.STUDENT_RESULT) {
-      return showResponsesTo.filter((visibilityType: FeedbackVisibilityType) =>
-          visibilityType !== FeedbackVisibilityType.INSTRUCTORS).length > 0;
+      return (
+        showResponsesTo.filter(
+          (visibilityType: FeedbackVisibilityType) =>
+            visibilityType !== FeedbackVisibilityType.INSTRUCTORS
+        ).length > 0
+      );
     }
     if (this.intent === Intent.INSTRUCTOR_RESULT) {
-      return showResponsesTo.filter((visibilityType: FeedbackVisibilityType) =>
-          visibilityType === FeedbackVisibilityType.INSTRUCTORS).length > 0;
+      return (
+        showResponsesTo.filter(
+          (visibilityType: FeedbackVisibilityType) =>
+            visibilityType === FeedbackVisibilityType.INSTRUCTORS
+        ).length > 0
+      );
     }
     return false;
   }
@@ -85,44 +94,52 @@ export class QuestionResponsePanelComponent {
       // Do not re-fetch data
       return;
     }
-    this.feedbackSessionsService.getFeedbackSessionResults({
-      questionId: question.feedbackQuestion.feedbackQuestionId,
-      courseId: this.session.courseId,
-      feedbackSessionName: this.session.feedbackSessionName,
-      intent: this.intent,
-      key: this.regKey,
-      previewAs: this.previewAsPerson,
-    }).subscribe({
-      next: (sessionResults: SessionResults) => {
-        const responses: QuestionOutput = sessionResults.questions[0];
-        if (responses) {
-          question.feedbackQuestion = responses.feedbackQuestion;
-          question.allResponses = responses.allResponses;
-          question.otherResponses = responses.otherResponses;
-          question.questionStatistics = responses.questionStatistics;
-          question.responsesFromSelf = responses.responsesFromSelf;
-          question.responsesToSelf = responses.responsesToSelf;
-          question.hasResponseButNotVisibleForPreview = responses.hasResponseButNotVisibleForPreview;
-          question.hasCommentNotVisibleForPreview = responses.hasCommentNotVisibleForPreview;
-        } else {
-          question.hasResponse = false;
-          if (question.errorMessage) {
-            this.statusMessageService.showSuccessToast('Question '
-              .concat(question.feedbackQuestion.questionNumber.toString())
-              .concat(' has no responses.'));
+    this.feedbackSessionsService
+      .getFeedbackSessionResults({
+        questionId: question.feedbackQuestion.feedbackQuestionId,
+        courseId: this.session.courseId,
+        feedbackSessionName: this.session.feedbackSessionName,
+        intent: this.intent,
+        key: this.regKey,
+        previewAs: this.previewAsPerson,
+      })
+      .subscribe({
+        next: (sessionResults: SessionResults) => {
+          const responses: QuestionOutput = sessionResults.questions[0];
+          if (responses) {
+            question.feedbackQuestion = responses.feedbackQuestion;
+            question.allResponses = responses.allResponses;
+            question.otherResponses = responses.otherResponses;
+            question.questionStatistics = responses.questionStatistics;
+            question.responsesFromSelf = responses.responsesFromSelf;
+            question.responsesToSelf = responses.responsesToSelf;
+            question.hasResponseButNotVisibleForPreview =
+              responses.hasResponseButNotVisibleForPreview;
+            question.hasCommentNotVisibleForPreview =
+              responses.hasCommentNotVisibleForPreview;
+            question.hasResponse = true;
+          } else {
+            question.hasResponse = false;
+            if (question.errorMessage) {
+              this.statusMessageService.showSuccessToast(
+                'Question '
+                  .concat(question.feedbackQuestion.questionNumber.toString())
+                  .concat(' has no responses.')
+              );
+            }
           }
-        }
-      },
-      complete: () => {
-        question.isLoaded = true;
-        question.isLoading = false;
-        question.errorMessage = '';
-      },
-      error: (resp: ErrorMessageOutput) => {
-        question.errorMessage = resp.error.message;
-        this.statusMessageService.showErrorToast(resp.error.message);
-      },
-    });
+        },
+        complete: () => {
+          question.isLoaded = true;
+          question.isLoading = false;
+          question.errorMessage = '';
+        },
+        error: (resp: ErrorMessageOutput) => {
+          question.errorMessage = resp.error.message;
+          this.statusMessageService.showErrorToast(resp.error.message);
+          console.log('error: ', resp.error.message);
+        },
+      });
   }
 
   loadQuestion(event: any, question: FeedbackQuestionModel): void {
